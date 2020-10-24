e62df3a0ef17d62a105733c27c4d7d5d7cd70bdf
Gaetan <gaetan@hexadog.com>
2020-10-24 11:10:41 +0200

    fix(nav): new versioning system

---
+++ src/1.4/sidebar.js
@@ -0,0 +1,31 @@
+module.exports = [{
+    "title": "Package",
+    "collapsable": false,
+    "children": [
+      "",
+      "installation",
+      "how-it-works"
+    ]
+  },
+  {
+    "title": "Usage",
+    "collapsable": false,
+    "children": [
+      "usage/basic",
+      "usage/middleware",
+      "usage/assets",
+      "usage/components",
+      "usage/error-views",
+      "usage/package-views"
+    ]
+  },
+  {
+    "title": "Configuration",
+    "collapsable": false,
+    "children": [
+      "configuration/directory",
+      "configuration/assets",
+      "configuration/fallback-theme"
+    ]
+  }
+];
---
+++ src/1.5/sidebar.js
@@ -0,0 +1,33 @@
+module.exports = [{
+    "title": "Package",
+    "collapsable": false,
+    "children": [
+      "",
+      "installation",
+      "how-it-works"
+    ]
+  },
+  {
+    "title": "Usage",
+    "collapsable": false,
+    "children": [
+      "usage/basic",
+      "usage/middleware",
+      "usage/assets",
+      "usage/components",
+      "usage/error-views",
+      "usage/package-views",
+      "usage/artisan"
+    ]
+  },
+  {
+    "title": "Configuration",
+    "collapsable": false,
+    "children": [
+      "configuration/directory",
+      "configuration/assets",
+      "configuration/fallback-theme",
+      "configuration/cache"
+    ]
+  }
+];
