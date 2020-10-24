2551baf6117965687321914e167ae8b22f038a17
Gaetan <gaetan@hexadog.com>
2020-10-24 10:15:30 +0200

    fix(nav): next version navigation

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -74,32 +74,32 @@ module.exports = {
         title: 'Package',
         collapsable: false,
         children: [
-          '',
-          'installation',
-          'how-it-works'
+          '/',
+          '/installation',
+          '/how-it-works'
         ]
       },
       {
         title: 'Usage',
         collapsable: false,
         children: [
-          'usage/basic',
-          'usage/middleware',
-          'usage/assets',
-          'usage/components',
-          'usage/error-views',
-          'usage/package-views',
-          'usage/artisan'
+          '/usage/basic',
+          '/usage/middleware',
+          '/usage/assets',
+          '/usage/components',
+          '/usage/error-views',
+          '/usage/package-views',
+          '/usage/artisan'
         ]
       },
       {
         title: 'Configuration',
         collapsable: false,
         children: [
-          'configuration/directory',
-          'configuration/assets',
-          'configuration/fallback-theme',
-          'configuration/cache'
+          '/configuration/directory',
+          '/configuration/assets',
+          '/configuration/fallback-theme',
+          '/configuration/cache'
         ]
       }
     ]
