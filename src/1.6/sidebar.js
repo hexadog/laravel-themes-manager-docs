88e0093d556ae35177f53faa7190171d9361da0d
Gaetan <gaetan@hexadog.com>
2020-10-25 10:10:53 +0100

    feat(version): publish 1.6 version

---
+++ src/1.6/sidebar.js
@@ -0,0 +1,32 @@
+module.exports = [{
+    "title": "Package",
+    "collapsable": false,
+    "children": [
+      "installation",
+      "how-it-works"
+    ]
+  },
+  {
+    "title": "Usage",
+    "collapsable": false,
+    "children": [
+      "usage/basic",
+      "usage/middleware",
+      "usage/assets",
+      "usage/views",
+      "usage/components",
+      "usage/artisan"
+    ]
+  },
+  {
+    "title": "Configuration",
+    "collapsable": false,
+    "children": [
+      "configuration/directory",
+      "configuration/assets",
+      "configuration/fallback-theme",
+      "configuration/cache",
+      "configuration/generator"
+    ]
+  }
+];
